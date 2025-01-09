f03410434a0d6dbfb29c9576becb54e6ad65b0ee
darkmoon <57591350+K2528358037@users.noreply.github.com>
2025-01-09 21:26:18 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -36,6 +36,8 @@ TARGET_DEVICES += lemonPi_pi1" >> target/linux/rockchip/image/rk35xx.mk
 
 
 cp -f $GITHUB_WORKSPACE/configfiles/rk3588-lemonpi-pi1.dts target/linux/rockchip/dts/rk3588
+ls target/linux/rockchip/dts/rk3588
+cat target/linux/rockchip/image/rk35xx.mk
 
 
 
